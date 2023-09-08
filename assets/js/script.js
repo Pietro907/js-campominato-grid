de18aedb034a41afd43ac5ffd6d297dd9a317758
Pietro907 <pietrop.puri@gmail.com>
2023-09-08 18:07:33 +0200

    add finish work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,85 @@
+
 /*
+
  Consegna
 L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
 Ogni cella ha un numero progressivo, da 1 a 100.
+
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Consigli del giorno: :party_wizard:
-Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
- */
 
-console.log('Hello World!');
-const list = ['Starwars', 'Matrix', 'Imitation game'];  //Run
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. 
 
-
-/* Leggila ma eseguila tra 2 secondi */
-for (let i = 0; i < list.length; i++) {
-    const element = list[i]; //Run x number of time
-    console.log(element);  //Run x number of time
-}
-
-console.log('Hello World 2!');  //Run (not runnin if the for loop is still running)
-
-if (10 >20) {
-    console.log('test 123');  //Run
-}
-
-
-function calcAge(yob) { //Run
-    
-console.log('test 2345');  //Run
-
-}
-
-
-/*
-Asyncronous Programming
 */
 
+const generateGrid = document.getElementById("genera_grid");
+const resetGrid = document.getElementById("reset_grid");
+const fieldElement = document.querySelector(".field");
+const limit = 100;
 
-// callback
-setTimeout(myFunction, 3000);
 
-function myFunction() {
-    alert('Hello')
-    console.log('Inside first alert 1');
+function generateMineField(Element, limit) {
+
+    console.log("Cella generata");
+
+    for (let i = 0; i < limit; i++) {
+        
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        Element.append(cellElement);
+        const cellValue = i + 1;
+
+        //l'addEventListener viene associato a ogni cella ogni volta che viene creata durante il ciclo e rimane in attesa.
+        cellElement.addEventListener("click", function () {
+
+            
+            this.classList.toggle("color_green");
+            console.log(cellValue);
+
+        })
+
+    }
+};
+
+//Funzione per rimuovere le mine
+function removeMineField(limit) {
+
+    console.log("removing cells");
+
+
+    fieldElement.innerHTML = "";
+
 }
-console.log('After the timing function1');
 
-//closure
-setTimeout(function(){
-   //code to run 3s 
-   console.log('inside the timing function 2');
-}, 3000);
+//Genera griglia
+generateGrid.addEventListener("click", function () {
 
-console.log('After the timing function 2');
+    
+    generateGrid.classList.toggle("active");
 
+    if (generateGrid.classList.contains("active")) {
 
-
-// interval
+        generateMineField(fieldElement, limit);
 
 
-const clock = setInterval(myFunction_2, 3000);
+    }
+});
 
-console.log(clock);
+//Reset griglia
+resetGrid.addEventListener("click", function () {
 
-function myFunction_2(){
-    console.log('Hello')
-}
+    if (generateGrid.classList.contains("active")) {
+        
+        console.log("resetting the field");
+
+        removeMineField(limit);
+
+        
+
+    }
+
+});
+
+
+
+
